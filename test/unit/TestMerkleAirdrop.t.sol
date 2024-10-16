ee1798d063b8fd173d6879ab3700a5902f622022
Shawn Rizo <shawnrizo@hotmail.com>
2024-10-16 18:13:18 -0400

    Feat: Murky Merkle Script Files Setup

---
+++ test/unit/TestMerkleAirdrop.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+
+contract TestMerkleAirdrop is Test {
+    MerkleAirdrop merkleAirdrop;
+
+    function setUp() public {}
+}
